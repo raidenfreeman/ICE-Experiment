f435444f4a107169ffd87433558ea89e678fa773
Raiden Freeman <raidenfreeman@hotmail.com>
2019-01-13 13:18:37 +0200

    Test 1

--- ICE-ICE-BABY/Form1.Designer.cs
+++ ICE-ICE-BABY/Form1.Designer.cs
@@ -28,13 +28,202 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.idText = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.outputText = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.startServer = new System.Windows.Forms.Button();
+            this.connectToServer = new System.Windows.Forms.Button();
+            this.idInput = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.chatInput = new System.Windows.Forms.TextBox();
+            this.chatOutput = new System.Windows.Forms.RichTextBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // idText
+            // 
+            this.idText.Location = new System.Drawing.Point(499, 54);
+            this.idText.Name = "idText";
+            this.idText.ReadOnly = true;
+            this.idText.Size = new System.Drawing.Size(200, 20);
+            this.idText.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(564, 37);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(75, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Connection ID";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(112, 100);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Do your magic!";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // outputText
+            // 
+            this.outputText.Location = new System.Drawing.Point(130, 12);
+            this.outputText.Multiline = true;
+            this.outputText.Name = "outputText";
+            this.outputText.ReadOnly = true;
+            this.outputText.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.outputText.Size = new System.Drawing.Size(345, 100);
+            this.outputText.TabIndex = 3;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(705, 54);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(25, 20);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "📝";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // startServer
+            // 
+            this.startServer.Location = new System.Drawing.Point(69, 61);
+            this.startServer.Name = "startServer";
+            this.startServer.Size = new System.Drawing.Size(112, 46);
+            this.startServer.TabIndex = 5;
+            this.startServer.Text = "Start";
+            this.startServer.UseVisualStyleBackColor = true;
+            this.startServer.Visible = false;
+            this.startServer.Click += new System.EventHandler(this.startServer_Click);
+            // 
+            // connectToServer
+            // 
+            this.connectToServer.Location = new System.Drawing.Point(37, 48);
+            this.connectToServer.Name = "connectToServer";
+            this.connectToServer.Size = new System.Drawing.Size(113, 46);
+            this.connectToServer.TabIndex = 6;
+            this.connectToServer.Text = "Connect to Server";
+            this.connectToServer.UseVisualStyleBackColor = true;
+            this.connectToServer.Visible = false;
+            // 
+            // idInput
+            // 
+            this.idInput.Location = new System.Drawing.Point(12, 35);
+            this.idInput.Name = "idInput";
+            this.idInput.Size = new System.Drawing.Size(200, 20);
+            this.idInput.TabIndex = 7;
+            this.idInput.TextChanged += new System.EventHandler(this.idInput_TextChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(72, 19);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(109, 13);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Target Connection ID";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.button4);
+            this.groupBox1.Controls.Add(this.idInput);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.startServer);
+            this.groupBox1.Location = new System.Drawing.Point(12, 118);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(254, 123);
+            this.groupBox1.TabIndex = 9;
+            this.groupBox1.TabStop = false;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(218, 35);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(25, 20);
+            this.button4.TabIndex = 10;
+            this.button4.Text = "📝";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // chatInput
+            // 
+            this.chatInput.Location = new System.Drawing.Point(275, 382);
+            this.chatInput.Name = "chatInput";
+            this.chatInput.Size = new System.Drawing.Size(200, 20);
+            this.chatInput.TabIndex = 10;
+            this.chatInput.Visible = false;
+            this.chatInput.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.chatInput_KeyPress);
+            // 
+            // chatOutput
+            // 
+            this.chatOutput.Location = new System.Drawing.Point(275, 118);
+            this.chatOutput.Name = "chatOutput";
+            this.chatOutput.ReadOnly = true;
+            this.chatOutput.Size = new System.Drawing.Size(200, 258);
+            this.chatOutput.TabIndex = 11;
+            this.chatOutput.Text = "";
+            this.chatOutput.Visible = false;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.connectToServer);
+            this.groupBox2.Location = new System.Drawing.Point(24, 262);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(200, 100);
+            this.groupBox2.TabIndex = 12;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "groupBox2";
+            this.groupBox2.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.chatOutput);
+            this.Controls.Add(this.chatInput);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.outputText);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.idText);
+            this.Name = "Form1";
+            this.Text = "ICE ICE BABY";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox idText;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox outputText;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button startServer;
+        private System.Windows.Forms.Button connectToServer;
+        private System.Windows.Forms.TextBox idInput;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.TextBox chatInput;
+        private System.Windows.Forms.RichTextBox chatOutput;
+        private System.Windows.Forms.GroupBox groupBox2;
     }
 }
 
--- ICE-ICE-BABY/Form1.cs
+++ ICE-ICE-BABY/Form1.cs
@@ -1,10 +1,15 @@
-﻿using System;
+﻿using LumiSoft.Net.STUN.Client;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Net;
+using System.Net.Sockets;
 using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -12,9 +17,270 @@ namespace ICE_ICE_BABY
 {
     public partial class Form1 : Form
     {
+        const int portNumber = 43788;
+        byte[] byteBuffer = new byte[1024];
+        Regex ValidIpAddressRegex;
         public Form1()
         {
             InitializeComponent();
+            ValidIpAddressRegex = new Regex(@"^(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5]):[0-9]+$");
+        }
+
+        private string connectionID;
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Cursor = Cursors.WaitCursor;
+            connectToServer.Visible = false;
+            startServer.Visible = false;
+            chatInput.Visible = false;
+            chatOutput.Visible = false;
+            try
+            {
+
+                Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+                socket.Bind(new IPEndPoint(IPAddress.Any, portNumber));
+
+                STUN_Result result = STUN_Client.Query("stun1.l.google.com", 19302, socket);
+                PrintOutput("NAT Type: " + result.NetType.ToString());
+                PrintOutput("Local Endpoint: " + socket.LocalEndPoint.ToString());
+                if (result.NetType != STUN_NetType.UdpBlocked)
+                {
+                    PrintOutput("Public Endpoint: " + result.PublicEndPoint.ToString());
+                    var id = Base64Encode(result.PublicEndPoint.ToString());
+                    //Clipboard.SetText(id);
+                    idText.Text = id;
+                }
+                PrintOutput("=================");
+                socket.Close();
+            }
+            catch (Exception x)
+            {
+                MessageBox.Show(this, "Error: " + x.ToString(), "Error:", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            finally
+            {
+                this.Cursor = Cursors.Default;
+            }
+        }
+        delegate void PrintOutputCallback(string text);
+        void PrintOutput(string s)
+        {
+            if (this.outputText.InvokeRequired)
+            {
+                PrintOutputCallback d = new PrintOutputCallback(PrintOutput);
+                this.Invoke(d, new object[] { s });
+            }
+            else
+            {
+                outputText.AppendText(s + Environment.NewLine);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Clipboard.SetText(idText.Text);
+        }
+
+        Socket sk;
+        IPEndPoint targetEndpoint;
+
+        delegate void ChatAppendCallback(string text, bool isIncoming);
+        private void ChatAppend(string text, bool isIncoming)
+        {
+            if (this.chatOutput.InvokeRequired)
+            {
+                ChatAppendCallback d = new ChatAppendCallback(ChatAppend);
+                this.Invoke(d, new object[] { text, isIncoming });
+            }
+            else
+            {
+                text += Environment.NewLine;
+                if (isIncoming)
+                {
+                    text += Environment.NewLine;
+                }
+
+                chatOutput.SelectionStart = chatOutput.TextLength;
+                chatOutput.SelectionLength = 0;
+
+                chatOutput.SelectionColor = isIncoming ? Color.Salmon : Color.SpringGreen;
+                chatOutput.SelectionAlignment = HorizontalAlignment.Right;
+                chatOutput.AppendText(text);
+                chatOutput.SelectionColor = chatOutput.ForeColor;
+            }
+        }
+
+        private void OnSend(IAsyncResult ar)
+        {
+            try
+            {
+                sk.EndSend(ar);
+                PrintOutput($"Sent: {chatInput.Text}");
+                ChatAppend(chatInput.Text, false);
+            }
+            catch (ObjectDisposedException)
+            { }
+            catch (Exception ex)
+            {
+                PrintOutput(ex.Message);
+            }
+            if (InvokeRequired)
+            {
+                this.Invoke(new Action(() => chatInput.Clear()));
+                return;
+            }
+        }
+
+        private void startServer_Click(object sender, EventArgs e)
+        {
+            var targetIP = IPAddress.Parse(Base64Decode(idInput.Text).Split(':')[0]);
+            var targetPort = Convert.ToInt32(Base64Decode(idInput.Text).Split(':')[1]);
+            targetEndpoint = new IPEndPoint(targetIP, targetPort);
+
+            if (sk != null)
+            {
+                sk.Close();
+            }
+            sk = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+
+            // Bind the socket to the local endpoint and listen for incoming connections.  
+            try
+            {
+                sk.Bind(new IPEndPoint(IPAddress.Any, portNumber));
+                PrintOutput($"🖥 Started server at port:{portNumber}");
+
+                // target: 85.72.40.234:43788  base64=> ODUuNzIuNDAuMjM0OjQzNzg4
+                PrintOutput($"👊 Hole punching...");
+                // Punchthrough
+                byte[] bytes = Encoding.ASCII.GetBytes("ggwp");
+                for (int i = 0; i < 10; i++)
+                {
+                    // Send a few packets to the target, to punch a hole in our NAT
+                    sk.SendTo(bytes, targetEndpoint);
+                    //s.Send(bytes, bytes.Length, targetEndpoint);
+                }
+                PrintOutput($"Done");
+
+                //TODO: CHANGE THIS TO TARGET ENDPOINT 😱😱😱😱😱😱😱😱😱😱😱😱😱😱😱😱😱😱😱😱😱😱😱
+                IPEndPoint ipeSender = new IPEndPoint(IPAddress.Any, 0);
+                //The epSender identifies the incoming clients
+                EndPoint epSender = (EndPoint)ipeSender;
+                //Start receiving data
+                sk.BeginReceiveFrom(byteBuffer, 0, byteBuffer.Length,
+                    SocketFlags.None, ref epSender, new AsyncCallback(OnReceive), epSender);
+
+                PrintOutput($"👂 Listenning for data...");
+                chatInput.Visible = true;
+                chatOutput.Visible = true;
+            }
+            catch (Exception ex)
+            {
+                PrintOutput("Something bad happened 😱");
+                PrintOutput(ex.Message);
+                Console.WriteLine(ex.ToString());
+                sk.Close();
+                PrintOutput("🔌 Connection closed");
+                connectToServer.Visible = false;
+                startServer.Visible = false;
+                chatInput.Visible = false;
+                chatOutput.Visible = false;
+            }
+        }
+
+        private void OnReceive(IAsyncResult ar)
+        {
+            EndPoint epSender = (EndPoint)targetEndpoint;
+            try
+            {
+
+                sk.EndReceiveFrom(ar, ref epSender);
+
+                var msgReceived = Encoding.ASCII.GetString(byteBuffer);
+
+                ChatAppend(msgReceived, true);
+                PrintOutput("Got: " + msgReceived);
+                byteBuffer = new byte[1024];
+
+                //Start listening to receive more data from the user
+                sk.BeginReceiveFrom(byteBuffer, 0, byteBuffer.Length, SocketFlags.None, ref epSender,
+                                           new AsyncCallback(OnReceive), null);
+            }
+            catch (Exception ex)
+            {
+                PrintOutput(ex.Message);
+                sk.Close();
+                PrintOutput("🔌 Connection closed");
+            }
+        }
+
+
+        private void idInput_TextChanged(object sender, EventArgs e)
+        {
+            var target = Base64Decode(idInput.Text);
+
+            var isMatch = ValidIpAddressRegex.IsMatch(target);
+            connectToServer.Visible = isMatch;
+            startServer.Visible = isMatch;
+            chatInput.Visible = false;
+            chatOutput.Visible = false;
+
+        }
+
+        public static string Base64Encode(string plainText)
+        {
+            var plainTextBytes = System.Text.Encoding.UTF8.GetBytes(plainText);
+            return System.Convert.ToBase64String(plainTextBytes);
+        }
+
+        public static string Base64Decode(string base64EncodedData)
+        {
+            var base64EncodedBytes = System.Convert.FromBase64String(base64EncodedData);
+            return System.Text.Encoding.UTF8.GetString(base64EncodedBytes);
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            if (sk != null)
+            {
+                sk.Close();
+            }
+        }
+        bool t = false;
+
+        private void chatInput_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == Convert.ToChar(Keys.Enter))
+            {
+                var textToSend = chatInput.Text;
+                if (textToSend != String.Empty)
+                {
+                    if (sk != null)
+                    {
+                        try
+                        {
+                            byte[] byteData = Encoding.ASCII.GetBytes(chatInput.Text);
+
+                            //Send it to the target
+                            sk.BeginSendTo(byteData, 0, byteData.Length, SocketFlags.None, targetEndpoint, new AsyncCallback(OnSend), null);
+                        }
+                        catch (Exception)
+                        {
+                            PrintOutput("Unable to send message to the target.");
+                        }
+                    }
+                    //else
+                    //{
+                    //    ChatAppend(textToSend, t);
+                    //    t = !t;
+                    //}
+                }
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            idInput.Text = Clipboard.GetText();
         }
     }
 }
